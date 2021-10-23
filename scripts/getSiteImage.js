4aa275367d810bfaeed9c76a08137e80d54ac574
tiago-rodrigues1 <tiagords127@gmail.com>
2021-10-23 14:58:49 -0300

    add modulo para tirar print da pagina desejada

---
+++ scripts/getSiteImage.js
@@ -0,0 +1,19 @@
+const puppeteer = require("puppeteer");
+
+async function getSiteImage(url) {
+	console.log("\n- Indo buscar imagem do site 📷");
+
+	try {
+		const browser = await puppeteer.launch({ headless: false });
+		const page = await browser.newPage();
+		await page.goto(url);
+
+		await page.screenshot({ path: "siteImg.png" });
+
+		await browser.close();
+	} catch (err) {
+		throw new Error("- Não foi possível pegar a imagem do site 😕 \n", err);
+	}
+}
+
+module.exports = getSiteImage;

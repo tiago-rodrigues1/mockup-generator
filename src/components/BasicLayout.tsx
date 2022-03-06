b727c3238f4262e3be250553b33d621d7142ef0a
tiago-rodrigues1 <tiagords127@gmail.com>
2022-03-06 12:07:07 -0300

    style: add relative class

--- src/components/BasicLayout.tsx
+++ src/components/BasicLayout.tsx
@@ -7,7 +7,7 @@ interface BasicLayoutProps {
 export const BasicLayout = ({ children }: BasicLayoutProps) => {
 	return (
 		<>
-			<div className="min-h-screen w-screen p-4 sm:p-8 flex flex-col gap-10 sm:gap-20 bg-neutral-900 text-neutral-50">
+			<div className="min-h-screen w-screen p-4 sm:p-8 flex flex-col gap-10 sm:gap-20 bg-neutral-900 text-neutral-50 relative">
 				{children}
 			</div>
 		</>

f89e95d71570732631e3f54ebe3572069074f5ad
tiago-rodrigues1 <tiagords127@gmail.com>
2022-04-16 09:38:18 -0300

    feat: add GenetateMethodCard component

---
+++ src/components/GenerateMethodCard.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { FiArrowRight } from "react-icons/fi";
+
+export const GeneraMethodCard = (props) => {
+	return (
+		<>
+			<Link href={props.redirectTo}>
+				<a className="w-full sm:w-1/3 h-auto flex flex-col bg-[#202020] rounded-lg duration-200 hover:scale-105">
+					<div className="flex-1 p-6 grid place-items-center">
+						<Image
+							src={props.imageSrc}
+							alt={`Modo de geração por ${props.methodName}`}
+							width={320}
+							height={320}
+							className="object-center object-fill my-0 mx-auto"
+						/>
+					</div>
+					<div className="bg-[#101010] w-full h-2/5 p-4 flex items-center justify-between rounded-b-lg">
+						<span>{props.methodName}</span>
+						<FiArrowRight size={24} />
+					</div>
+				</a>
+			</Link>
+		</>
+	);
+};
